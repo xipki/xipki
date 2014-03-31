f2e76512a7e1629581990dac207fa26234165e57
xipki <lijun.liao@gmail.com>
2014-03-31 19:05:18 +0200

    adapt the license header

--- ca/ca-server/src/main/java/org/xipki/ca/server/mgmt/AddLicense.java
+++ ca/ca-server/src/main/java/org/xipki/ca/server/mgmt/AddLicense.java
@@ -1,24 +1,17 @@
 /*
- * ====================================================================
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright 2014 xipki.org
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- * ====================================================================
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * This work is part of XiPKI, owned by Lijun Liao (lijun.liao@gmail.com)
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
  *
  */
 
@@ -34,26 +27,19 @@ public class AddLicense {
 	
 	private final static String licenseText =
 	 "/*\n" +
-	 " * ====================================================================\n" +
-	 " * Licensed to the Apache Software Foundation (ASF) under one\n" +
-	 " * or more contributor license agreements.  See the NOTICE file\n" +
-	 " * distributed with this work for additional information\n" +
-	 " * regarding copyright ownership.  The ASF licenses this file\n" +
-	 " * to you under the Apache License, Version 2.0 (the\n" +
-	 " * \"License\"); you may not use this file except in compliance\n" +
-	 " * with the License.  You may obtain a copy of the License at\n" +
+	 " * Copyright 2014 xipki.org\n" +
 	 " *\n" +
-	 " *   http://www.apache.org/licenses/LICENSE-2.0\n" +
+	 " * Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+	 " * you may not use this file except in compliance with the License.\n" +
+	 " * You may obtain a copy of the License at\n" +
 	 " *\n" +
-	 " * Unless required by applicable law or agreed to in writing,\n" +
-	 " * software distributed under the License is distributed on an\n" +
-	 " * \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n" +
-	 " * KIND, either express or implied.  See the License for the\n" +
-	 " * specific language governing permissions and limitations\n" +
-	 " * under the License.\n" +
-	 " * ====================================================================\n" +
+	 " *         http://www.apache.org/licenses/LICENSE-2.0\n" +
 	 " *\n" +
-	 " * This work is part of XiPKI, owned by Lijun Liao (lijun.liao@gmail.com)\n" +
+	 " * Unless required by applicable law or agreed to in writing, software\n" +
+	 " * distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+	 " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+	 " * See the License for the specific language governing permissions and\n" +
+	 " * limitations under the License\n" +
 	 " *\n" +
 	 " */\n\n";
 	
