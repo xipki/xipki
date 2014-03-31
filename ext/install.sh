fc22b2f9463c42e00ac71025178d032ed0f4a461
xipki <lijun.liao@gmail.com>
2014-03-31 17:46:41 +0200

    centralized the management of iaikPkcs11Wrapper

--- ext/install.sh
+++ ext/install.sh
@@ -2,3 +2,6 @@
 
 mvn install:install-file -Dfile=iaikPkcs11Wrapper-1.2.18.jar -DgroupId=at.tugraz.iaik \
   -DartifactId=iaikPkcs11Wrapper -Dversion=1.2.18 -Dpackaging=jar
+
+mvn install:install-file -Dfile=iaikPkcs11Wrapper-native-1.2.18.zip -DgroupId=at.tugraz.iaik \
+  -DartifactId=iaikPkcs11Wrapper-native -Dversion=1.2.18 -Dpackaging=zip
