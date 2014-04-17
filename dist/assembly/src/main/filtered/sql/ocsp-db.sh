4e7bfc824e57d311318f709269870113e166ba1a
xipki <lijun.liao@gmail.com>
2014-04-17 23:36:26 +0200

    Changing lookup mechanism of JDBC drivers

--- dist/assembly/src/main/filtered/sql/ocsp-db.sh
+++ dist/assembly/src/main/filtered/sql/ocsp-db.sh
@@ -4,10 +4,10 @@ COMMAND=$1
 
 PROP_FILE=ocsp-db.properties
 CHANGELOG_FILE=ocsp-init.xml
-JDBC_CP=../system/mysql/mysql-connector-java/${mysql-jdbc.version}/mysql-connector-java-${mysql-jdbc.version}.jar
+JDBC_CP=$( echo ../lib/ext/*.jar | sed 's/ /:/g')
 
 # retrieve the database configuration
-cat ../ca-demo/$PROP_FILE | sed 's/\./_/' > .$PROP_FILE
+cat ../ocsp-config/$PROP_FILE | sed 's/\./_/' > .$PROP_FILE
 . ./.$PROP_FILE
 rm .$PROP_FILE -f
 echo command=$COMMAND
