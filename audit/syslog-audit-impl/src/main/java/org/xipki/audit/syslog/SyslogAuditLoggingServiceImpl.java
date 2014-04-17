95afe2447964242f74b1f1b871d458edd7cffa5f
xipki <lijun.liao@gmail.com>
2014-04-17 12:20:56 +0200

    add license header

--- audit/syslog-audit-impl/src/main/java/org/xipki/audit/syslog/SyslogAuditLoggingServiceImpl.java
+++ audit/syslog-audit-impl/src/main/java/org/xipki/audit/syslog/SyslogAuditLoggingServiceImpl.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2014 xipki.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ *
+ */
+
 package org.xipki.audit.syslog;
 
 import java.nio.charset.Charset;
@@ -24,7 +41,7 @@ import com.nesscomputing.syslog4j.impl.message.pci.PCISyslogMessage;
 import com.nesscomputing.syslog4j.util.SyslogUtility;
 
 public class SyslogAuditLoggingServiceImpl implements AuditLoggingService
-{ 
+{
     private static final Logger LOG = LoggerFactory.getLogger(SyslogAuditLoggingServiceImpl.class);
     private static final DateFormat df = new SimpleDateFormat("yyyy.MM.dd '-' HH:mm:ss.SSS z");
 
