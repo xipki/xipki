b5648054787bcb4fc84af1685b85487b091ba84b
xipki <lijun.liao@gmail.com>
2014-04-25 21:19:07 +0200

    add (c) to the copyright line

--- security/security-core/src/main/java/org/xipki/security/p11/P11ECDSAKeyParameter.java
+++ security/security-core/src/main/java/org/xipki/security/p11/P11ECDSAKeyParameter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 xipki.org
+ * Copyright (c) 2014 xipki.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
