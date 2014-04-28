57e26f674a5c876090f74c6d7d2c681cd9ab67e8
xipki <lijun.liao@gmail.com>
2014-04-28 17:36:54 +0200

    let user confirm the database actions

--- dist/assembly/src/main/unfiltered/sql/ocsp-db.sh
+++ dist/assembly/src/main/unfiltered/sql/ocsp-db.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
+LIQUIBASE_HOME=liquibase
+
 COMMAND=$1
 
 PROP_FILE=ocsp-publisher.properties
 CHANGELOG_FILE=ocsp-init.xml
+
 JDBC_CP=$( echo ../lib/ext/*.jar | sed 's/ /:/g')
 
 # retrieve the database configuration
 cat ../ocsp-config/$PROP_FILE | sed 's/\./_/' > .$PROP_FILE
 . ./.$PROP_FILE
 rm .$PROP_FILE -f
-echo command=$COMMAND
 echo db.driverClassName=$db_driverClassName
 echo db.username=$db_username
-
 #echo db.password=$db_password
+
+
+DRIVER=$db_driverClassName
 
 DFLT_SCHEMA=
 
-if [ "$db_driverClassName" = "com.ibm.db2.jcc.DB2Driver" ]; then
+if [ "$DRIVER" = "com.ibm.db2.jcc.DB2Driver" ]; then
         echo "Database type: DB2"
         SEP=":currentSchema="
         db_schema=${db_url#*$SEP}
@@ -32,5 +36,32 @@ else
         echo db.url=$db_url
 fi
 
-liquibase/liquibase --url=$db_url --username=$db_username --password=$db_password --changeLogFile=$CHANGELOG_FILE --classpath=$JDBC_CP $DFLT_SCHEMA $COMMAND
+liquibase ()
+{
+   CMD="liquibase/liquibase --driver=$DRIVER --classpath=$JDBC_CP --changeLogFile=$CHANGELOG_FILE $DFLT_SCHEMA --url=$db_url --username=$db_username --password=$db_password $COMMAND"
+   #echo "$CMD"
+   $CMD
+}
+
+if [ $# == 1 ]; then
+  while true; do
+    read -p "Do you wish to $COMMAND database? y/n " RESP
+    if [ "$RESP" = "y" ]; then
+       liquibase
+       break
+    elif [ "$RESP" = "n" ]; then
+       break
+    else
+       echo "Please answer with y or n"
+    fi
+  done
+else
+  echo 'Usage: '
+  echo   'ocsp-db.sh <command>'
+  echo  ''
+  echo  'Command:'
+  echo     migrate
+  echo     update
+  echo     dropAll
+fi
 
