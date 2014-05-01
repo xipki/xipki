b21528809c041ea39a09211f481f709d89843143
xipki <lijun.liao@gmail.com>
2014-05-01 23:14:14 +0200

    1. ca: remove prefix 'file:' from the XSD location of dbchangelog-3.0.xsd 2. ocsp: use special exception class for OCSPResponse with status other than successfull 3. dist: optimize the OSGi features definition

--- dist/assembly/src/main/unfiltered/sql/init-db.sh
+++ dist/assembly/src/main/unfiltered/sql/init-db.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
+if [ "$LIQUIBASE_HOME" = "" ]; then
+  echo "LIQUIBASE_HOME is not set."
+  exit
+fi
+
+if [ "$XIPKI_HOME" = "" ]; then
+  echo "XIPKI_HOME is not set."
+  exit
+fi
+
 # Log level of liquibase: debug, info, warning, severe, off
 LOG_LEVEL=off
 
 COMMAND=$1
+PROP_FILE=$2
+CHANGELOG_FILE=$3
 
-PROP_FILE=ca-db.properties
-CHANGELOG_FILE=ca-init.xml
-
-JDBC_CP=$( echo ../lib/ext/*.jar | sed 's/ /:/g')
+JDBC_CP=$( echo $XIPKI_HOME/lib/ext/*.jar | sed 's/ /:/g')
 
 # retrieve the database configuration
-cat ../ca-config/$PROP_FILE | sed 's/\./_/' > .$PROP_FILE
-. ./.$PROP_FILE
-rm .$PROP_FILE -f
+cat $PROP_FILE | sed 's/\./_/' > .tmp-db.properties
+. ./.tmp-db.properties
+rm -f .tmp-db.properties
+
 echo db.driverClassName=$db_driverClassName
 echo db.username=$db_username
 #echo db.password=$db_password
@@ -24,7 +34,6 @@ DRIVER=$db_driverClassName
 DFLT_SCHEMA=
 
 if [ "$DRIVER" = "com.ibm.db2.jcc.DB2Driver" ]; then
-        echo "Database type: DB2"
         SEP=":currentSchema="
         db_schema=${db_url#*$SEP}
         db_url=${db_url%$SEP*}
@@ -33,18 +42,19 @@ if [ "$DRIVER" = "com.ibm.db2.jcc.DB2Driver" ]; then
         echo db.schema=$db_schema
         echo db.url=$db_url
 else
-        echo "Database type: -"
         echo db.url=$db_url
 fi
 
+echo "changelog: $CHANGELOG_FILE"
+
 liquibase ()
 {
-   CMD="liquibase/liquibase --driver=$DRIVER --classpath=$JDBC_CP --changeLogFile=$CHANGELOG_FILE $DFLT_SCHEMA --url=$db_url --username=$db_username --password=$db_password --logLevel=$LOG_LEVEL $COMMAND"
+   CMD="$LIQUIBASE_HOME/liquibase --driver=$DRIVER --classpath=$JDBC_CP --changeLogFile=$CHANGELOG_FILE $DFLT_SCHEMA --url=$db_url --username=$db_username --password=$db_password --logLevel=$LOG_LEVEL $COMMAND"
    #echo "$CMD"
    $CMD
 }
 
-if [ $# = 1 ]; then
+if [ $# = 3 ]; then
   while true; do
     read -p "Do you wish to $COMMAND database? y/n " RESP
     if [ "$RESP" = "y" ]; then
@@ -58,10 +68,9 @@ if [ $# = 1 ]; then
   done
 else
   echo 'Usage: '
-  echo   'ca-db.sh <command>'
+  echo   'ca-db.sh <command> <database conf file> <changlog file>'
   echo  ''
   echo  'Command:'
-  echo     migrate
   echo     update
   echo     dropAll
 fi
--- dist/assembly/src/main/unfiltered/sql/reset.sh
+++ dist/assembly/src/main/unfiltered/sql/reset.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
 
-./ca-db.sh dropAll
+## resolve links - $0 may be a symlink
+PRG="$0"
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+   PRG=`dirname "$PRG"`"/$link"
+  fi
+done
+
+THIS_HOME=`dirname "$PRG"`
+
+# make it fully qualified
+export XIPKI_HOME=`cd "$THIS_HOME/.." && pwd`
+export LIQUIBASE_HOME="$XIPKI_HOME/sql/liquibase"
+
+DBCONF_FILE=$XIPKI_HOME/ca-config/ca-db.properties
+CHANGELOG_FILE=$XIPKI_HOME/sql/ca-init.xml
+
+$THIS_HOME/init-db.sh dropAll $DBCONF_FILE $CHANGELOG_FILE
 echo ""
 echo ""
 
-./ca-db.sh update
+$THIS_HOME/init-db.sh update $DBCONF_FILE $CHANGELOG_FILE
 echo ""
 echo ""
 
-./ocsp-db.sh dropAll
+DBCONF_FILE=$XIPKI_HOME/ocsp-config/ocsp-publisher.properties
+CHANGELOG_FILE=$XIPKI_HOME/sql/ocsp-init.xml
+
+$THIS_HOME/init-db.sh dropAll $DBCONF_FILE $CHANGELOG_FILE
 echo ""
 echo ""
 
-./ocsp-db.sh update
-echo ""
-echo ""
+$THIS_HOME/init-db.sh update $DBCONF_FILE $CHANGELOG_FILE
+
