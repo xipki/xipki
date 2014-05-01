1c7be57712c744d25beb0f4955f07c5178141f32
xipki <lijun.liao@gmail.com>
2014-05-01 23:14:14 +0200

    1. ca: remove prefix 'file:' from the XSD location of dbchangelog-3.0.xsd 2. ocsp: use special exception class for OCSPResponse with status other than successfull 3. dist: optimize the OSGi features definition

---
+++ ocsp/ocsp-client-api/src/main/java/org/xipki/ocsp/client/api/OCSPResponseNotSuccessfullException.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2014 xipki.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ *
+ */
+
+package org.xipki.ocsp.client.api;
+
+public class OCSPResponseNotSuccessfullException extends OCSPRequestorException
+{
+
+    private static final long serialVersionUID = 1L;
+    private final int statusCode;
+    private final String statusText;
+
+    public OCSPResponseNotSuccessfullException(int statusCode)
+    {
+        super("OCSPResponse with status " + statusCode + " (" + getOCSPResponseStatus(statusCode) + ")");
+        this.statusCode = statusCode;
+        this.statusText = getOCSPResponseStatus(statusCode);
+    }
+
+    public int getStatusCode()
+    {
+        return statusCode;
+    }
+
+    public String getStatusText()
+    {
+        return statusText;
+    }
+
+    private static String getOCSPResponseStatus(int statusCode)
+    {
+        switch(statusCode)
+        {
+        case 0:
+            return "successfull";
+        case 1:
+            return "malformedRequest";
+        case 2:
+            return "internalError";
+        case 3:
+            return "tryLater";
+        case 5:
+            return "sigRequired";
+        case 6:
+            return "unauthorized";
+        default:
+            return "undefined";
+        }
+    }
+
+}
