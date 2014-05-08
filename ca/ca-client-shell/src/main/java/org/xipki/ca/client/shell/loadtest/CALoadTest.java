285b122ebf435ca05166dc29ab9bd836398ec8cc
xipki <lijun.liao@gmail.com>
2014-05-08 21:57:28 +0200

    add CA loadtest for EC key

--- ca/ca-client-shell/src/main/java/org/xipki/ca/client/shell/loadtest/CALoadTest.java
+++ ca/ca-client-shell/src/main/java/org/xipki/ca/client/shell/loadtest/CALoadTest.java
@@ -19,18 +19,31 @@ package org.xipki.ca.client.shell.loadtest;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.cert.X509Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Calendar;
 import java.util.Locale;
 
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.crmf.CertRequest;
 import org.bouncycastle.asn1.crmf.CertTemplate;
 import org.bouncycastle.asn1.crmf.CertTemplateBuilder;
 import org.bouncycastle.asn1.crmf.ProofOfPossession;
 import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.asn1.x9.X9ECPoint;
+import org.bouncycastle.asn1.x9.X9ObjectIdentifiers;
 import org.bouncycastle.crypto.util.SubjectPublicKeyInfoFactory;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
+import org.bouncycastle.math.ec.ECFieldElement;
+import org.bouncycastle.math.ec.ECFieldElement.F2m;
+import org.bouncycastle.math.ec.ECPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xipki.ca.client.api.RAWorker;
@@ -44,9 +57,127 @@ import org.xipki.ca.common.RAWorkerException;
 import org.xipki.security.SignerUtil;
 import org.xipki.security.common.AbstractLoadTest;
 import org.xipki.security.common.ParamChecker;
+import org.xipki.security.p10.P12KeypairGenerator;
 
-public class CALoadTest extends AbstractLoadTest
+abstract class CALoadTest extends AbstractLoadTest
 {
+	static class RSACALoadTest extends CALoadTest
+	{
+	    private BigInteger baseN;
+	
+		public RSACALoadTest(RAWorker raWorker, String certProfile,
+				String commonNamePrefix, String otherPartOfSubject, int keysize)
+		{
+			super(raWorker, certProfile, commonNamePrefix, otherPartOfSubject);
+			if(keysize % 1024 != 0)
+			{
+				throw new IllegalArgumentException("invalid RSA keysize " + keysize);
+			}
+			
+	        this.baseN = BigInteger.valueOf(0);
+	        this.baseN = this.baseN.setBit(keysize - 1);
+	        for(int i = 32; i < keysize - 1; i+= 2)
+	        {
+	            this.baseN = this.baseN.setBit(i);
+	        }
+		}
+
+		@Override
+		protected SubjectPublicKeyInfo getSubjectPublicKeyInfo(long index)
+		{
+	        BigInteger modulus = baseN.add(BigInteger.valueOf(index));
+
+	        try
+	        {
+	            return SubjectPublicKeyInfoFactory.createSubjectPublicKeyInfo(
+	                    SignerUtil.generateRSAPublicKeyParameter(
+	                            org.xipki.security.KeyUtil.generateRSAPublicKey(modulus,
+	                                    BigInteger.valueOf(65535))));
+	        } catch (InvalidKeySpecException e)
+	        {
+	            LOG.warn("InvalidKeySpecException: {}", e.getMessage());
+	            return null;
+	        } catch (IOException e)
+	        {
+	            LOG.warn("IOException: {}", e.getMessage());
+	            return null;
+	        }
+		}
+	}
+
+	static class ECCALoadTest extends CALoadTest
+	{
+		private ASN1ObjectIdentifier curveOid;
+		private String curveName;
+		private ECPoint baseQ; 
+		
+		public ECCALoadTest(RAWorker raWorker, String certProfile,
+				String commonNamePrefix, String otherPartOfSubject, String curveNameOrOid)
+		throws Exception
+		{
+			super(raWorker, certProfile, commonNamePrefix, otherPartOfSubject);			
+            boolean isOid;
+            try
+            {
+                new ASN1ObjectIdentifier(curveNameOrOid);
+                isOid = true;
+            }catch(Exception e)
+            {
+                isOid = false;
+            }
+
+            if(isOid)
+            {
+                this.curveOid = new ASN1ObjectIdentifier(curveNameOrOid);
+                this.curveName = P12KeypairGenerator.ECDSAIdentityGenerator.getCurveName(this.curveOid);
+            }
+            else
+            {
+                this.curveName = curveNameOrOid;
+                this.curveOid = P12KeypairGenerator.ECDSAIdentityGenerator.getCurveOID(this.curveName);
+                if(this.curveOid == null)
+                {
+                    throw new IllegalArgumentException("No OID is defined for the curve " + this.curveName);
+                }
+            }
+
+            KeyPairGenerator kpgen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            ECNamedCurveParameterSpec spec = ECNamedCurveTable.getParameterSpec(curveName);
+            kpgen.initialize(spec);
+            KeyPair kp = kpgen.generateKeyPair();
+            this.baseQ = ((BCECPublicKey) kp.getPublic()).getQ();
+		}
+
+		@Override
+		protected SubjectPublicKeyInfo getSubjectPublicKeyInfo(long index) 
+		{
+            AlgorithmIdentifier algId = new AlgorithmIdentifier(X9ObjectIdentifiers.id_ecPublicKey,
+                    this.curveOid);
+            
+            ECPoint q;
+            if(baseQ instanceof ECPoint.F2m)
+            {
+            	ECFieldElement.F2m basePointX = (ECFieldElement.F2m) ((ECPoint.F2m) baseQ).getX();
+                BigInteger x = basePointX.toBigInteger().add(BigInteger.valueOf(index));
+            	
+                ECFieldElement.F2m pointX = new F2m(basePointX.getM(),basePointX.getK1(),
+                		basePointX.getK2(), basePointX.getK3(), x);
+            	q = new ECPoint.F2m(baseQ.getCurve(), pointX, baseQ.getY());
+            }
+            else //if(baseQ instanceof ECPoint.Fp)
+            {
+            	ECFieldElement.Fp basePointX = (ECFieldElement.Fp) ((ECPoint.Fp) baseQ).getX();
+            	BigInteger x = basePointX.toBigInteger().add(BigInteger.valueOf(index));
+            	ECFieldElement.Fp pointX = new ECFieldElement.Fp(basePointX.getQ(), x);
+            	
+            	q = new ECPoint.Fp(baseQ.getCurve(), pointX, baseQ.getY());
+            }
+            
+           ASN1OctetString p = (ASN1OctetString)new X9ECPoint(q).toASN1Primitive();
+           return new SubjectPublicKeyInfo(algId, p.getOctets());
+		}
+	}
+	
     private static final Logger LOG = LoggerFactory.getLogger(CALoadTest.class);
 
     private final RAWorker raWorker;
@@ -55,8 +186,9 @@ public class CALoadTest extends AbstractLoadTest
     private final String otherPartOfSubject;
 
     private long index;
-    private BigInteger baseN;
-
+    
+    protected abstract SubjectPublicKeyInfo getSubjectPublicKeyInfo(long index);
+    
     @Override
     protected Runnable getTestor()
     throws Exception
@@ -77,13 +209,6 @@ public class CALoadTest extends AbstractLoadTest
         this.certProfile = certProfile;
         this.commonNamePrefix = commonNamePrefix;
         this.otherPartOfSubject = otherPartOfSubject;
-
-        this.baseN = BigInteger.valueOf(0);
-        this.baseN = this.baseN.setBit(2047);
-        for(int i = 32; i < 2047; i+= 2)
-        {
-            this.baseN = this.baseN.setBit(i);
-        }
 
         Calendar baseTime = Calendar.getInstance(Locale.UK);
         baseTime.set(Calendar.YEAR, 2014);
@@ -107,24 +232,12 @@ public class CALoadTest extends AbstractLoadTest
         X500Name subject = new X500Name("CN=" + commonNamePrefix + thisIndex + "," + otherPartOfSubject);
         certTempBuilder.setSubject(subject);
 
-        BigInteger modulus = baseN.add(BigInteger.valueOf(thisIndex));
-
-        SubjectPublicKeyInfo spki;
-        try
+        SubjectPublicKeyInfo spki = getSubjectPublicKeyInfo(index);
+        if(spki == null)
         {
-            spki = SubjectPublicKeyInfoFactory.createSubjectPublicKeyInfo(
-                    SignerUtil.generateRSAPublicKeyParameter(
-                            org.xipki.security.KeyUtil.generateRSAPublicKey(modulus,
-                                    BigInteger.valueOf(65535))));
-        } catch (InvalidKeySpecException e)
-        {
-            LOG.warn("InvalidKeySpecException: {}", e.getMessage());
-            return null;
-        } catch (IOException e)
-        {
-            LOG.warn("IOException: {}", e.getMessage());
-            return null;
+        	return null;
         }
+        
         certTempBuilder.setPublicKey(spki);
 
         CertTemplate certTemplate = certTempBuilder.build();
