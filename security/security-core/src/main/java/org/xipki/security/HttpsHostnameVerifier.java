36a0e574027e6c820f8aa47d59ad2aac6f2056b9
xipki <lijun.liao@gmail.com>
2014-05-08 16:56:44 +0200

    average result over 10 seconds

--- security/security-core/src/main/java/org/xipki/security/HttpsHostnameVerifier.java
+++ security/security-core/src/main/java/org/xipki/security/HttpsHostnameVerifier.java
@@ -60,7 +60,7 @@ public class HttpsHostnameVerifier implements HostnameVerifier
 
     public void shutdown()
     {
-        if(meAsDefaultHostnameVerifier)
+        if(meAsDefaultHostnameVerifier && HttpsURLConnection.getDefaultHostnameVerifier() == this)
         {
             LOG.info("Unregister me as DefaultHostnameVerifier, and reuse the old one " + oldHostnameVerifier);
             HttpsURLConnection.setDefaultHostnameVerifier(oldHostnameVerifier);
