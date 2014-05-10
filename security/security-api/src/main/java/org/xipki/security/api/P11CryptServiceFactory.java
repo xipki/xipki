87347fa1e0fc63b9297c40675950352c327ac150
xipki <lijun.liao@gmail.com>
2014-05-10 18:32:37 +0200

    corrected bugs that updated certificate will be not found after refresh, add option to specify slots to be considered of PKCS#11 devices

--- security/security-api/src/main/java/org/xipki/security/api/P11CryptServiceFactory.java
+++ security/security-api/src/main/java/org/xipki/security/api/P11CryptServiceFactory.java
@@ -17,8 +17,14 @@
 
 package org.xipki.security.api;
 
+import java.util.Set;
+
 public interface P11CryptServiceFactory
 {
     P11CryptService createP11CryptService(String pkcs11Module, char[] password)
+    throws SignerException;
+
+    P11CryptService createP11CryptService(String pkcs11Module, char[] password,
+            Set<Integer> includeSlotIndexes, Set<Integer> excludeSlotIndexes)
     throws SignerException;
 }
--- security/security-iaikp11/src/main/java/org/xipki/security/p11/iaik/IaikP11CryptServiceFactory.java
+++ security/security-iaikp11/src/main/java/org/xipki/security/p11/iaik/IaikP11CryptServiceFactory.java
@@ -17,6 +17,8 @@
 
 package org.xipki.security.p11.iaik;
 
+import java.util.Set;
+
 import org.xipki.security.api.P11CryptService;
 import org.xipki.security.api.P11CryptServiceFactory;
 import org.xipki.security.api.SignerException;
@@ -29,7 +31,15 @@ public class IaikP11CryptServiceFactory implements P11CryptServiceFactory
             char[] password)
     throws SignerException
     {
-        return IaikP11CryptService.getInstance(pkcs11Module, password);
+        return createP11CryptService(pkcs11Module, password, null, null);
+    }
+
+    @Override
+    public P11CryptService createP11CryptService(String pkcs11Module, char[] password,
+            Set<Integer> includeSlotIndexes, Set<Integer> excludeSlotIndexes)
+    throws SignerException
+    {
+        return IaikP11CryptService.getInstance(pkcs11Module, password, includeSlotIndexes, excludeSlotIndexes);
     }
 
 }
