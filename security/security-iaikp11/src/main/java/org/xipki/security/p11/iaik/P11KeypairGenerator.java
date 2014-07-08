4b6332f0a862cdd8375713a61a3f9b8e922bb41e
xipki <lijun.liao@gmail.com>
2014-07-08 12:02:22 +0200

    better format

--- security/security-iaikp11/src/main/java/org/xipki/security/p11/iaik/P11KeypairGenerator.java
+++ security/security-iaikp11/src/main/java/org/xipki/security/p11/iaik/P11KeypairGenerator.java
@@ -36,7 +36,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
