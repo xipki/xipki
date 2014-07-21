a739a26ce0181718054dd9fba7e3cd641389ed89
xipki <lijun.liao@gmail.com>
2014-07-21 11:44:59 +0200

    add karaf shell to unrevoke CA, fixed bug in the karaf shell to revoke CA

---
+++ ca/ca-server-shell/src/main/java/org/xipki/ca/server/mgmt/shell/CaUnrevokeCommand.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014 Lijun Liao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ *
+ */
+
+package org.xipki.ca.server.mgmt.shell;
+
+import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
+
+/**
+ * @author Lijun Liao
+ */
+
+@Command(scope = "ca", name = "ca-unrevoke", description="Unrevoke CA")
+public class CaUnrevokeCommand extends CaCommand
+{
+    @Option(name = "-name",
+            description = "Required, CA name",
+            required = true)
+    protected String           caName;
+
+    @Override
+    protected Object doExecute()
+    throws Exception
+    {
+        if(caManager.getCANames().contains(caName) == false)
+        {
+            System.out.println("invalid CA name " + caName);
+            return null;
+        }
+
+        caManager.unrevokeCa(caName);
+
+        return null;
+    }
+}
