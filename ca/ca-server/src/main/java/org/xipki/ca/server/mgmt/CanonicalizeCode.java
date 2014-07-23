b6fc7c727c8c95cb794374c32dd5e54ad0b84a29
xipki <lijun.liao@gmail.com>
2014-07-23 21:22:25 +0200

    remove the license header

--- ca/ca-server/src/main/java/org/xipki/ca/server/mgmt/CanonicalizeCode.java
+++ ca/ca-server/src/main/java/org/xipki/ca/server/mgmt/CanonicalizeCode.java
@@ -1,17 +1,7 @@
 /*
  * Copyright (c) 2014 Lijun Liao
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
+ * TO-BE-DEFINE
  *
  */
 
@@ -35,7 +25,7 @@ public class CanonicalizeCode
 {
 
     private final static String licenseText =
-     "/*\n" +
+/*     "/*\n" +
      " * Copyright (c) 2014 Lijun Liao\n" +
      " *\n" +
      " * Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
@@ -49,6 +39,13 @@ public class CanonicalizeCode
      " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
      " * See the License for the specific language governing permissions and\n" +
      " * limitations under the License\n" +
+     " *\n" +
+     " *//*\n\n";
+*/
+     "/*\n" +
+     " * Copyright (c) 2014 Lijun Liao\n" +
+     " *\n" +
+     " * TO-BE-DEFINE\n" +
      " *\n" +
      " */\n\n";
 
