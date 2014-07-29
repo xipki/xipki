04675e6d6cbd828488649a3170ac1fa2b14a2579
xipki <lijun.liao@gmail.com>
2014-07-29 11:03:05 +0200

    add karaf shell command to export certprofile to file

---
+++ ca/ca-server-shell/src/main/java/org/xipki/ca/server/mgmt/shell/ProfileExportCommand.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2014 Lijun Liao
+ *
+ * TO-BE-DEFINE
+ *
+ */
+
+package org.xipki.ca.server.mgmt.shell;
+
+import java.io.File;
+
+import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
+import org.xipki.ca.server.mgmt.CertProfileEntry;
+
+/**
+ * @author Lijun Liao
+ */
+
+@Command(scope = "ca", name = "profile-export", description="Export profile configuration")
+public class ProfileExportCommand extends CaCommand
+{
+    @Option(name = "-name",
+            description = "Required. Profile name",
+            required = true, multiValued = false)
+    protected String name;
+
+    @Option(name = "-out",
+            description = "Required. Where to save the profile configuration",
+            required = true)
+    protected String confFile;
+
+    @Override
+    protected Object doExecute()
+    throws Exception
+    {
+        CertProfileEntry entry = caManager.getCertProfile(name);
+        if(entry == null)
+        {
+            System.out.println("No cert profile named " + name + " is defined");
+            return null;
+        }
+
+        saveVerbose("Saved cert profile configuration to", new File(confFile), entry.getConf().getBytes("UTF-8"));
+        return null;
+    }
+}
