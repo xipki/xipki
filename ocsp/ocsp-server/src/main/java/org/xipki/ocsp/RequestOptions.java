2dc9ec8279410a8d969155e2387a5ec704e47ffb
xipki <lijun.liao@gmail.com>
2014-08-01 08:10:32 +0200

    add support of validating OCSP request signature

--- ocsp/ocsp-server/src/main/java/org/xipki/ocsp/RequestOptions.java
+++ ocsp/ocsp-server/src/main/java/org/xipki/ocsp/RequestOptions.java
@@ -7,13 +7,31 @@
 
 package org.xipki.ocsp;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertStore;
+import java.security.cert.CertStoreParameters;
+import java.security.cert.CertificateException;
+import java.security.cert.CollectionCertStoreParameters;
+import java.security.cert.TrustAnchor;
+import java.security.cert.X509Certificate;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.xipki.ocsp.conf.jaxb.CertCollectionType;
+import org.xipki.ocsp.conf.jaxb.CertCollectionType.Keystore;
 import org.xipki.ocsp.conf.jaxb.NonceType;
 import org.xipki.ocsp.conf.jaxb.RequestType;
 import org.xipki.ocsp.conf.jaxb.RequestType.HashAlgorithms;
 import org.xipki.security.common.HashAlgoType;
+import org.xipki.security.common.IoCertUtil;
 
 /**
  * @author Lijun Liao
@@ -39,6 +57,8 @@ class RequestOptions
     private final int nonceMinLen;
     private final int nonceMaxLen;
     private final Set<HashAlgoType> hashAlgos;
+    private final Set<TrustAnchor> trustAnchors;
+    private final CertStore certs;
 
     public RequestOptions(RequestType conf)
     throws OcspResponderException
@@ -96,6 +116,51 @@ class RequestOptions
             hashAlgos.addAll(supportedHashAlgorithms);
         }
 
+        // keystore
+        CertCollectionType trustStoreType = conf.getTrustAnchors();
+        if(trustStoreType == null)
+        {
+            if(validateSignature)
+            {
+                throw new OcspResponderException("TrustAnchor is not specified");
+            }
+            trustAnchors = null;
+            certs = null;
+        }
+        else
+        {
+            try
+            {
+                Set<X509Certificate> certs = getCerts(trustStoreType);
+                Set<TrustAnchor> _trustAnchors = new HashSet<TrustAnchor>();
+                for(X509Certificate cert : certs)
+                {
+                    _trustAnchors.add(new TrustAnchor(cert, null));
+                }
+                this.trustAnchors = Collections.unmodifiableSet(_trustAnchors);
+            }catch(Exception e)
+            {
+                throw new OcspResponderException("Error while initializing the trustAnchors: " + e.getMessage(), e);
+            }
+
+            CertCollectionType certsType = conf.getCerts();
+            if(certsType == null)
+            {
+                this.certs = null;
+            }
+            else
+            {
+                try
+                {
+                    CertStoreParameters csp = new CollectionCertStoreParameters(getCerts(certsType));
+                    this.certs = CertStore.getInstance("Collection", csp, "BC");
+                }catch(Exception e)
+                {
+                    throw new OcspResponderException("Error while initializing the certs: " + e.getMessage(), e);
+                }
+            }
+        }
+
     }
 
     public Set<HashAlgoType> getHashAlgos()
@@ -131,6 +196,68 @@ class RequestOptions
     public boolean allows(HashAlgoType hashAlgo)
     {
         return hashAlgos.contains(hashAlgo);
+    }
+
+    public Set<TrustAnchor> getTrustAnchors()
+    {
+        return trustAnchors;
+    }
+
+    public CertStore getCerts()
+    {
+        return certs;
+    }
+
+    private Set<X509Certificate> getCerts(CertCollectionType conf)
+    throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException
+    {
+        Set<X509Certificate> certs = new HashSet<>();
+
+        if(conf.getKeystore() != null)
+        {
+            Keystore ksConf = conf.getKeystore();
+            KeyStore trustStore = KeyStore.getInstance(ksConf.getType());
+            InputStream is = null;
+
+            String uriS = ksConf.getUri();
+            if(uriS != null)
+            {
+                URL uri = new URL(uriS);
+                is = uri.openStream();
+            }
+
+            char[] password = ksConf.getPassword() == null ?
+                    null : ksConf.getPassword().toCharArray();
+            trustStore.load(is, password);
+
+            Enumeration<String> aliases = trustStore.aliases();
+            while(aliases.hasMoreElements())
+            {
+                String alias = aliases.nextElement();
+                if(trustStore.isCertificateEntry(alias))
+                {
+                    certs.add((X509Certificate) trustStore.getCertificate(alias));
+                }
+            }
+        }
+        else if(conf.getDir() != null)
+        {
+            File dir = new File(conf.getDir());
+            File[] files = dir.listFiles();
+            for(File file : files)
+            {
+                if(file.exists() && file.isFile())
+                {
+                    certs.add(IoCertUtil.parseCert(file));
+                }
+            }
+        }
+        else
+        {
+            throw new RuntimeException("should not happen, neither keystore nor dir is defined");
+        }
+
+        return certs;
     }
 
 }
