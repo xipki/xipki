15266940bb5d0801a812c61f3a0a5d3c806acd5d
xipki <lijun.liao@gmail.com>
2014-08-05 22:18:06 +0200

    renamed ECCurverNameCompleter to ECCurveNameCompleter

---
+++ security/security-shell/src/main/java/org/xipki/security/shell/completer/ECCurveNameCompleter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2014 Lijun Liao
+ *
+ * TO-BE-DEFINE
+ *
+ */
+
+package org.xipki.security.shell.completer;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bouncycastle.asn1.nist.NISTNamedCurves;
+import org.bouncycastle.asn1.sec.SECNamedCurves;
+import org.bouncycastle.asn1.teletrust.TeleTrusTNamedCurves;
+import org.bouncycastle.asn1.x9.X962NamedCurves;
+import org.xipki.console.karaf.DynamicEnumCompleter;
+
+/**
+ * @author Lijun Liao
+ */
+
+public class ECCurveNameCompleter extends DynamicEnumCompleter
+{
+
+    @Override
+    protected Set<String> getEnums()
+    {
+        Set<String> curveNames = new HashSet<>();
+        Enumeration<?> names = X962NamedCurves.getNames();
+        while(names.hasMoreElements())
+        {
+            curveNames.add((String) names.nextElement());
+        }
+
+        names = SECNamedCurves.getNames();
+        while(names.hasMoreElements())
+        {
+            curveNames.add((String) names.nextElement());
+        }
+
+        names = TeleTrusTNamedCurves.getNames();
+        while(names.hasMoreElements())
+        {
+            curveNames.add((String) names.nextElement());
+        }
+
+        names = NISTNamedCurves.getNames();
+        while(names.hasMoreElements())
+        {
+            curveNames.add((String) names.nextElement());
+        }
+
+        return curveNames;
+    }
+
+}
