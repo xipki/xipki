c3884c4cbeb6131ab7aa2d0522a1b711a50b902e
xipki <lijun.liao@gmail.com>
2014-09-06 21:44:38 +0200

    Add support of operations of one master CA instance and multiple slave CA instances:   the serial number will be generated in the DB

--- ca/ca-common/src/main/java/org/xipki/ca/common/CASystemStatus.java
+++ ca/ca-common/src/main/java/org/xipki/ca/common/CASystemStatus.java
@@ -13,28 +13,10 @@ package org.xipki.ca.common;
 
 public enum CASystemStatus
 {
-    STARTED (0),
-    NOT_INITED (1),
-    INITIALIZING (2),
-    LOCK_FAILED (3),
-    ERROR (4);
-
-    private final int code;
-
-    private CASystemStatus(int code)
-    {
-        this.code = code;
-    }
-
-    public int getCode()
-    {
-        return code;
-    }
-
-    @Override
-    public String toString()
-    {
-        return code + ": " + name();
-    }
-
+    STARTED_AS_MASTER,
+    STARTED_AS_SLAVE,
+    NOT_INITED,
+    INITIALIZING,
+    LOCK_FAILED,
+    ERROR;
 }
