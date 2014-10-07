f9e1dc8dd6b4816327b15586959688d86cedd3b4
xipki <lijun.liao@gmail.com>
2014-10-07 22:01:49 +0200

    add feature to delete expired certificates

---
+++ ca/ca-common/src/main/java/org/xipki/ca/common/BadRequestException.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014 Lijun Liao
+ *
+ * TO-BE-DEFINE
+ *
+ */
+
+package org.xipki.ca.common;
+
+/**
+ * @author Lijun Liao
+ */
+
+public class BadRequestException extends Exception
+{
+
+    private static final long serialVersionUID = 1L;
+
+    public BadRequestException()
+    {
+    }
+
+    public BadRequestException(String message)
+    {
+        super(message);
+    }
+
+    public BadRequestException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public BadRequestException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+}
