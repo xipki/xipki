98e7d39c73a6544c88e63fdbd82d23ee05661d33
xipki <lijun.liao@gmail.com>
2014-12-05 20:37:02 +0100

    update IAIK PKCS#11 wrapper to version 1.3

--- ext/install.sh
+++ ext/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-mvn install:install-file -Dfile=iaikPkcs11Wrapper-1.2.18.jar -DgroupId=at.tugraz.iaik \
-  -DartifactId=iaikPkcs11Wrapper -Dversion=1.2.18 -Dpackaging=jar
+VERSION=1.3.0
 
-mvn install:install-file -Dfile=iaikPkcs11Wrapper-native-1.2.18.zip -DgroupId=at.tugraz.iaik \
-  -DartifactId=iaikPkcs11Wrapper-native -Dversion=1.2.18 -Dpackaging=zip
+mvn install:install-file -Dfile=iaikPkcs11Wrapper-${VERSION}.jar -DgroupId=at.tugraz.iaik \
+  -DartifactId=iaikPkcs11Wrapper -Dversion=${VERSION} -Dpackaging=jar
+
+mvn install:install-file -Dfile=iaikPkcs11Wrapper-native-${VERSION}.zip -DgroupId=at.tugraz.iaik \
+  -DartifactId=iaikPkcs11Wrapper-native -Dversion=${VERSION} -Dpackaging=zip
