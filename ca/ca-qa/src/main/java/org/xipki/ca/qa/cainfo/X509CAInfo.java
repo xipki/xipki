18b34f69ad13f0b54466f11a2efe346c8a4c0eaf
xipki <lijun.liao@gmail.com>
2014-12-20 20:20:18 +0100

    added module to check whether the given certificate match the certificate profile description. (NOT FINISHED)

---
+++ ca/ca-qa/src/main/java/org/xipki/ca/qa/cainfo/X509CAInfo.java
@@ -0,0 +1,108 @@
+/*
+ *
+ * This file is part of the XiPKI project.
+ * Copyright (c) 2014 Lijun Liao
+ * Author: Lijun Liao
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+ * THE AUTHOR LIJUN LIAO. LIJUN LIAO DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+ * OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the XiPKI software without
+ * disclosing the source code of your own applications.
+ *
+ * For more information, please contact Lijun Liao at this
+ * address: lijun.liao@gmail.com
+ */
+
+package org.xipki.ca.qa.cainfo;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import org.bouncycastle.asn1.x509.Certificate;
+import org.bouncycastle.util.Arrays;
+import org.xipki.common.ParamChecker;
+import org.xipki.common.SecurityUtil;
+
+/**
+ * @author Lijun Liao
+ */
+
+public class X509CAInfo
+{
+    private final String ocspURL;
+    private final String crlURL;
+    private final String deltaCrlURL;
+    private final X509Certificate cert;
+    private final Certificate bcCert;
+    private final byte[] ski;
+
+    public X509CAInfo(String ocspURL, String crlURL, String deltaCrlURL, byte[] certBytes)
+    throws CertificateException
+    {
+        ParamChecker.assertNotNull("certBytes", certBytes);
+        this.ocspURL = ocspURL;
+        this.crlURL = crlURL;
+        this.deltaCrlURL = deltaCrlURL;
+        try
+        {
+            this.cert = SecurityUtil.parseCert(certBytes);
+        } catch (IOException e)
+        {
+            throw new CertificateException(e.getMessage(), e);
+        }
+        this.bcCert = Certificate.getInstance(certBytes);
+        this.ski = SecurityUtil.extractSKI(cert);
+    }
+
+    public String getOcspURL()
+    {
+        return ocspURL;
+    }
+
+    public String getCrlURL()
+    {
+        return crlURL;
+    }
+
+    public String getDeltaCrlURL()
+    {
+        return deltaCrlURL;
+    }
+
+    public X509Certificate getCert()
+    {
+        return cert;
+    }
+
+    public byte[] getSubjectKeyIdentifier()
+    {
+        return Arrays.clone(ski);
+    }
+
+    public Certificate getBcCert()
+    {
+        return bcCert;
+    }
+
+}
