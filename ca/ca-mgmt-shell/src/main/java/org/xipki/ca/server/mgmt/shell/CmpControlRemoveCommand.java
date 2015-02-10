656f282db45670cb188452840930949645beb97e
xipki <lijun.liao@gmail.com>
2015-02-10 21:17:40 +0100

    better management of CMP control

--- ca/ca-mgmt-shell/src/main/java/org/xipki/ca/server/mgmt/shell/CmpControlRemoveCommand.java
+++ ca/ca-mgmt-shell/src/main/java/org/xipki/ca/server/mgmt/shell/CmpControlRemoveCommand.java
@@ -42,7 +42,7 @@ import org.apache.karaf.shell.commands.Command;
  * @author Lijun Liao
  */
 
-@Command(scope = "xipki-ca", name = "cmpcontrol-remove", description="Remove CMP control")
+@Command(scope = "xipki-ca", name = "cmpcontrol-rm", description="Remove CMP control")
 public class CmpControlRemoveCommand extends CaCommand
 {
     @Argument(index = 0, name = "name", description = "CMP control name", required = true)
