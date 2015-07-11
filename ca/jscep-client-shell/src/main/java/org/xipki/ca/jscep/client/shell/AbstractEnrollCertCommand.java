a243b5d601e99c4cf937168603a862462483ad66
Lijun Liao <lijun.liao@gmail.com>
2015-07-11 23:15:51 +0200

    corrected the messageData of SCEP PkcsRep from SignedData to ContentInfo add scripts to test the SCEP with thirdparty software JSCEP

---
+++ ca/jscep-client-shell/src/main/java/org/xipki/ca/jscep/client/shell/AbstractEnrollCertCommand.java
@@ -0,0 +1,106 @@
+/*
+ *
+ * This file is part of the XiPKI project.
+ * Copyright (c) 2014 - 2015 Lijun Liao
+ * Author: Lijun Liao
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+ * THE AUTHOR LIJUN LIAO. LIJUN LIAO DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+ * OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the XiPKI software without
+ * disclosing the source code of your own applications.
+ *
+ * For more information, please contact Lijun Liao at this
+ * address: lijun.liao@gmail.com
+ */
+
+package org.xipki.ca.jscep.client.shell;
+
+import java.io.File;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+
+import org.apache.karaf.shell.commands.Option;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.jscep.client.Client;
+import org.jscep.client.ClientException;
+import org.jscep.client.EnrollmentResponse;
+import org.jscep.transaction.TransactionException;
+import org.xipki.common.util.IoUtil;
+import org.xipki.console.karaf.CmdFailure;
+
+/**
+ * @author Lijun Liao
+ */
+
+public abstract class AbstractEnrollCertCommand extends ClientCommand
+{
+    @Option(name = "--p10",
+            required = true,
+            description = "PKCS#10 request file\n"
+                    + "(required)")
+    private String p10File;
+
+    @Option(name = "--out", aliases = "-o",
+            required = true,
+            description = "where to save the certificate\n"
+                    + "(required)")
+    private String outputFile;
+
+    protected abstract EnrollmentResponse requestCertificate(
+            Client client,
+            PKCS10CertificationRequest csr,
+            PrivateKey identityKey,
+            X509Certificate identityCert)
+    throws ClientException, TransactionException;
+
+    @Override
+    protected Object _doExecute()
+    throws Exception
+    {
+        Client client = getScepClient();
+
+        PKCS10CertificationRequest csr = new PKCS10CertificationRequest(IoUtil.read(p10File));
+
+        EnrollmentResponse resp = requestCertificate(client, csr, getIdentityKey(), getIdentityCert());
+        if(resp.isFailure())
+        {
+            throw new CmdFailure("server returned 'failure'");
+        }
+
+        if(resp.isPending())
+        {
+            throw new CmdFailure("server returned 'pending'");
+        }
+
+        X509Certificate cert = extractEECerts(resp.getCertStore());
+
+        if(cert == null)
+        {
+            throw new Exception("received no certificate");
+        }
+
+        saveVerbose("saved enrolled certificate to file", new File(outputFile), cert.getEncoded());
+        return null;
+    }
+
+}
