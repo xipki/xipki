57922e174617be78f01512c43bdd7561929aeed1
Lijun Liao <lijun.liao@gmail.com>
2016-03-21 23:13:38 +0100

    split the pkcs11-proxy code

--- commons/pkcs11-proxy-client/src/main/java/org/xipki/commons/pkcs11proxy/client/impl/RemoteP11Identity.java
+++ commons/pkcs11-proxy-client/src/main/java/org/xipki/commons/pkcs11proxy/client/impl/RemoteP11Identity.java
@@ -34,7 +34,7 @@
  * address: lijun.liao@gmail.com
  */
 
-package org.xipki.commons.pkcs11proxy.client;
+package org.xipki.commons.pkcs11proxy.client.impl;
 
 import java.security.PublicKey;
 import java.security.cert.X509Certificate;
