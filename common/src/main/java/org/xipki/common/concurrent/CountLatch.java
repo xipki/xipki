3cef8f01bb90e4b8c50d5d885068433e0e853b0a
Lijun Liao <lijun.liao@gmail.com>
2017-07-05 22:44:54 +0200

    add method to await with timeout

--- common/src/main/java/org/xipki/common/concurrent/CountLatch.java
+++ common/src/main/java/org/xipki/common/concurrent/CountLatch.java
@@ -34,6 +34,7 @@
 
 package org.xipki.common.concurrent;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
@@ -73,6 +74,11 @@ public class CountLatch {
 
     public void await() throws InterruptedException {
         sync.acquireSharedInterruptibly(1);
+    }
+
+    public boolean await(long timeout, TimeUnit unit)
+            throws InterruptedException {
+        return sync.tryAcquireSharedNanos(1, unit.toNanos(timeout));
     }
 
     public long countUp() {
