f03733cebedaf1ec2204a55472ce88963c3c52a0
Lijun Liao <lijun.liao@gmail.com>
2019-03-19 22:17:16 +0100

    reduce the size of binaries to be deployed.

--- assemblies/shared/dbtool/bin/initdb.bat
+++ assemblies/shared/dbtool/bin/initdb.bat
@@ -2,8 +2,23 @@
 
 setlocal
 
+set MYPWD=%cd%
 set DIRNAME=%~dp0%
 set ARGS=%*
+
+if not exist "%DIRNAME%\..\tmplib" (
+    mkdir "%DIRNAME%\..\tmplib"
+    cd "%DIRNAME%\..\tmplib"
+
+    if exist "..\..\webapps\ca.war" (
+        set WARFILE=..\..\webapps\ca.war
+    ) else (
+        set WARFILE=..\..\webapps\ocsp.war
+    )
+
+    jar xf %WARFILE% WEB-INF\lib
+    cd %MYPWD%
+)
 
 rem Check/Set up JAVA exec
 if "%JAVA_HOME%" == "" (
@@ -12,6 +27,8 @@ if "%JAVA_HOME%" == "" (
     set JAVA_EXEC="%JAVA_HOME%"\bin\java
 )
 
-%JAVA_EXEC% -cp "%DIRNAME%\..;%DIRNAME%\..\lib\*;%DIRNAME%\..\..\lib\*" org.xipki.dbtool.InitDbMain %ARGS%
+set CLASSPATH=%DIRNAME%\..\lib\*;%DIRNAME%\..\..\lib\*;%DIRNAME%\..\tmplib\WEB-INF\lib\*
+
+%JAVA_EXEC% -cp %CLASSPATH% org.xipki.dbtool.InitDbMain %ARGS%
 
 endlocal
--- assemblies/shared/dbtool/bin/initdb.sh
+++ assemblies/shared/dbtool/bin/initdb.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
+MYPWD=`pwd`
+
 DIRNAME=$(dirname "$0")
+
+if [ ! -d "$DIRNAME/../tmplib" ]; then
+	mkdir "$DIRNAME/../tmplib"
+	cd "$DIRNAME/../tmplib"
+
+	if [ -f "../../webapps/ca.war" ]; then
+		WARFILE=../../webapps/ca.war
+	else
+		WARFILE=../../webapps/ocsp.war
+	fi
+
+	jar xf $WARFILE WEB-INF/lib
+	cd $MYPWD
+fi
 
 if [ "x$JAVA_HOME" = "x" ]; then
      JAVA_EXEC=java
@@ -8,4 +24,6 @@ else
      JAVA_EXEC=$JAVA_HOME/bin/java
 fi
 
-$JAVA_EXEC -cp "$DIRNAME/..:$DIRNAME/../lib/*:$DIRNAME/../../lib/*" org.xipki.dbtool.InitDbMain "$@"
+CLASSPATH="$DIRNAME/../lib/*:$DIRNAME/../../lib/*:$DIRNAME/../tmplib/WEB-INF/lib/*"
+
+$JAVA_EXEC -cp "$CLASSPATH" org.xipki.dbtool.InitDbMain "$@"
