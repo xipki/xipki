17589872eba0e3390a3c5b1fceb81548c710173d
Lijun Liao <lijun.liao@gmail.com>
2021-12-11 17:07:02 +0100

    simplify the QA steps

--- assemblies/xipki-qa/src/main/unfiltered/xipki/prepare.sh
+++ assemblies/xipki-qa/src/main/unfiltered/xipki/prepare.sh
@@ -7,6 +7,14 @@ DIR=`dirname $0`
 
 echo "working dir: ${DIR}"
 
+rm -rf ${TOMCAT_DIR}/webapps/* ${TOMCAT_DIR}/logs/* ${TOMCAT_DIR}/xipki
+
+rm -rf ${TOMCAT_DIR}/lib/bc*.jar ${TOMCAT_DIR}/lib/mariadb*.jar ${TOMCAT_DIR}/lib/postgres*.jar
+
+cp -r xipki-ca/* ${TOMCAT_DIR}/
+
+cp -r xipki-ocsp/* ${TOMCAT_DIR}/
+
 cp -r ${DIR}/webapps/* ${TOMCAT_DIR}/webapps
 
 cp -r ${DIR}/etc/* ${XIPKI_DIR}/etc
