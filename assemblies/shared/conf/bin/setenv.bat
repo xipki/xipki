6e9b561b432ee81afdc8fbd4a453f8168f91b2db
Lijun Liao <lijun.liao@gmail.com>
2022-02-12 14:59:44 +0100

    add support of JDK 17 (except in CLI)

--- assemblies/shared/conf/bin/setenv.bat
+++ assemblies/shared/conf/bin/setenv.bat
@@ -1,1 +1,2 @@
 set JAVA_OPTS=%JAVA_OPTS% -DXIPKI_BASE=%CATALINA_HOME%\xipki
+set JDK_JAVA_OPTIONS=%JDK_JAVA_OPTIONS% --add-exports=jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED
--- assemblies/shared/conf/bin/setenv.sh
+++ assemblies/shared/conf/bin/setenv.sh
@@ -1,1 +1,2 @@
 export JAVA_OPTS="${JAVA_OPTS} -DXIPKI_BASE=${CATALINA_HOME}/xipki"
+export JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS} --add-exports=jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED"
