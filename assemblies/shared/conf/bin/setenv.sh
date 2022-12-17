973b8cb46532bcb19d4e91a548d522e9760ab703
Vladimir Vitkov <5081863+zeridon@users.noreply.github.com>
2022-12-17 22:31:51 +0200

    Update sentenv.sh (#268)
    Fixes https://github.com/xipki/xipki/issues/267

--- assemblies/shared/conf/bin/setenv.sh
+++ assemblies/shared/conf/bin/setenv.sh
@@ -1,2 +1,13 @@
-export JAVA_OPTS="${JAVA_OPTS} -DXIPKI_BASE=${CATALINA_HOME}/xipki"
+# Try to guess where xipki conf is
+if [ -d ${CATALINA_HOME}/xipki ] ; then
+	# try first the CATALINA_HOME - original
+	export JAVA_OPTS="${JAVA_OPTS} -DXIPKI_BASE=${CATALINA_HOME}/xipki"
+elif [ -d ${CATALINA_BASE}/xipki ] ; then
+	# try the CATALINA_BASE (usual setup on debian/ubuntu)
+	export JAVA_OPTS="${JAVA_OPTS} -DXIPKI_BASE=${CATALINA_BASE}/xipki"
+else
+	# Finally fall back to CATALINA_HOME (source install)
+	export JAVA_OPTS="${JAVA_OPTS} -DXIPKI_BASE=${CATALINA_HOME}/xipki"
+fi
+
 export JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS} --add-exports=jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED"
