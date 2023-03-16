fcfc00597fd447c76b5e5750118902162e880129
Lijun Liao <lijun.liao@gmail.com>
2023-03-16 20:21:52 +0100

    use the underlying PasswordResolver for the PBE-commands.

--- shells/security-shell/src/main/java/org/xipki/security/shell/PasswordActions.java
+++ shells/security-shell/src/main/java/org/xipki/security/shell/PasswordActions.java
@@ -20,11 +20,10 @@ package org.xipki.security.shell;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.Completion;
 import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.support.completers.FileCompleter;
-import org.xipki.password.OBFPasswordService;
-import org.xipki.password.PBEAlgo;
-import org.xipki.password.PBEPasswordService;
+import org.xipki.password.*;
 import org.xipki.security.shell.Actions.SecurityAction;
 import org.xipki.shell.IllegalCmdParamException;
 import org.xipki.util.Args;
@@ -122,6 +121,9 @@ public class PasswordActions {
   @Service
   public static class PbeDec extends SecurityAction {
 
+    @Reference
+    private PasswordResolver passwordResolver;
+
     @Option(name = "--password", description = "encrypted password, starts with PBE:\n"
             + "exactly one of password and password-file must be specified")
     private String passwordHint;
@@ -130,20 +132,12 @@ public class PasswordActions {
     @Completion(FileCompleter.class)
     private String passwordFile;
 
-    @Option(name = "--mpassword-file", description = "file containing the (obfuscated) master password")
-    @Completion(FileCompleter.class)
-    private String masterPasswordFile;
-
-    @Option(name = "--mk", description = "quorum of the master password parts")
-    private Integer mquorum = 1;
-
     @Option(name = "--out", description = "where to save the password")
     @Completion(FileCompleter.class)
     private String outFile;
 
     @Override
     protected Object execute0() throws Exception {
-      Args.range(mquorum, "mk", 1, 10);
       if ((passwordHint == null) == (passwordFile == null)) {
         throw new IllegalCmdParamException("exactly one of password and password-file must be specified");
       }
@@ -156,25 +150,7 @@ public class PasswordActions {
         throw new IllegalCmdParamException("encrypted password '" + passwordHint + "' does not start with PBE:");
       }
 
-      char[] masterPassword;
-      if (masterPasswordFile != null) {
-        String str = StringUtil.toUtf8String(IoUtil.read(masterPasswordFile));
-        if (StringUtil.startsWithIgnoreCase(str, OBFPasswordService.PROTOCOL_OBF + ":")) {
-          str = OBFPasswordService.deobfuscate(str);
-        }
-        masterPassword = str.toCharArray();
-      } else {
-        if (mquorum == 1) {
-          masterPassword = readPassword("Master password");
-        } else {
-          char[][] parts = new char[mquorum][];
-          for (int i = 0; i < mquorum; i++) {
-            parts[i] = readPassword("Master password (part " + (i + 1) + "/" + mquorum + ")");
-          }
-          masterPassword = StringUtil.merge(parts);
-        }
-      }
-      char[] password = PBEPasswordService.decryptPassword(masterPassword, passwordHint);
+      char[] password = passwordResolver.resolvePassword(passwordHint);
 
       if (outFile != null) {
         saveVerbose("saved the password to file", outFile, StringUtil.toUtf8Bytes(new String(password)));
@@ -190,8 +166,8 @@ public class PasswordActions {
   @Service
   public static class PbeEnc extends SecurityAction {
 
-    @Option(name = "--iteration-count", aliases = "-n", description = "iteration count, between 1 and 65535")
-    private int iterationCount = 2000;
+    @Reference
+    private PasswordResolver passwordResolver;
 
     @Option(name = "--out", description = "where to save the encrypted password")
     @Completion(FileCompleter.class)
@@ -200,37 +176,9 @@ public class PasswordActions {
     @Option(name = "-k", description = "quorum of the password parts")
     private Integer quorum = 1;
 
-    @Option(name = "--mpassword-file", description = "file containing the (obfuscated) master password")
-    @Completion(FileCompleter.class)
-    private String masterPasswordFile;
-
-    @Option(name = "--mk", description = "quorum of the master password parts")
-    private Integer mquorum = 1;
-
     @Override
     protected Object execute0() throws Exception {
-      Args.range(iterationCount, "iterationCount", 1, 65535);
       Args.range(quorum, "k", 1, 10);
-      Args.range(mquorum, "mk", 1, 10);
-
-      char[] masterPassword;
-      if (masterPasswordFile != null) {
-        String str = StringUtil.toUtf8String(IoUtil.read(masterPasswordFile));
-        if (StringUtil.startsWithIgnoreCase(str, OBFPasswordService.PROTOCOL_OBF + ":")) {
-          str = OBFPasswordService.deobfuscate(str);
-        }
-        masterPassword = str.toCharArray();
-      } else {
-        if (mquorum == 1) {
-          masterPassword = readPassword("Master password");
-        } else {
-          char[][] parts = new char[mquorum][];
-          for (int i = 0; i < mquorum; i++) {
-            parts[i] = readPassword("Master password (part " + (i + 1) + "/" + mquorum + ")");
-          }
-          masterPassword = StringUtil.merge(parts);
-        }
-      }
 
       char[] password;
       if (quorum == 1) {
@@ -243,8 +191,7 @@ public class PasswordActions {
         password = StringUtil.merge(parts);
       }
 
-      String passwordHint = PBEPasswordService.encryptPassword(PBEAlgo.PBEWithHmacSHA256AndAES_256,
-          iterationCount, masterPassword, password);
+      String passwordHint = passwordResolver.protectPassword(PBEPasswordService.PROTOCOL_PBE, password);
       if (outFile != null) {
         saveVerbose("saved the encrypted password to file", outFile, StringUtil.toUtf8Bytes(passwordHint));
       } else {
