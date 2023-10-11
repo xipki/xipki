5b7b966d62b22895d1406090e811f03216800a60
Lijun Liao <lijun.liao@gmail.com>
2023-10-11 21:07:26 +0200

    extend cmp client script to revoke/unsuspend multiple certificates.

--- shells/cmp-client-shell/src/main/java/org/xipki/cmp/client/shell/UnRevokeCertActions.java
+++ shells/cmp-client-shell/src/main/java/org/xipki/cmp/client/shell/UnRevokeCertActions.java
@@ -9,14 +9,14 @@ import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.support.completers.FileCompleter;
 import org.xipki.cmp.PkiStatusInfo;
-import org.xipki.cmp.client.CertIdOrError;
-import org.xipki.cmp.client.Requestor;
+import org.xipki.cmp.client.*;
 import org.xipki.security.CrlReason;
 import org.xipki.security.X509Cert;
 import org.xipki.security.util.X509Util;
 import org.xipki.shell.CmdFailure;
 import org.xipki.shell.Completers;
 import org.xipki.shell.IllegalCmdParamException;
+import org.xipki.util.CollectionUtil;
 import org.xipki.util.DateUtil;
 import org.xipki.util.ReqRespDebug;
 
@@ -25,6 +25,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.security.cert.CertificateException;
 import java.time.Instant;
+import java.util.*;
 
 /**
  * CMP client actions to revoke, unrevoke and remove certificates.
@@ -33,6 +34,18 @@ import java.time.Instant;
  *
  */
 public class UnRevokeCertActions {
+
+  private static class ReqInfo {
+
+    private X509Cert caCert;
+
+    private List<String> ids;
+
+    private List<BigInteger> serialNumbers;
+
+    private List<String> sources;
+
+  }
 
   @Command(scope = "xi", name = "cmp-revoke", description = "revoke certificate")
   @Service
@@ -47,46 +60,41 @@ public class UnRevokeCertActions {
 
     @Override
     protected Object execute0() throws Exception {
-      if ((certFile == null) == (getSerialNumber() == null)) {
-        throw new IllegalCmdParamException("exactly one of cert and serial must be specified");
-      }
-
       CrlReason crlReason = CrlReason.forNameOrText(reason);
 
       if (!CrlReason.PERMITTED_CLIENT_CRLREASONS.contains(crlReason)) {
         throw new IllegalCmdParamException("reason " + reason + " is not permitted");
       }
 
-      CertIdOrError certIdOrError;
-
       Instant invalidityDate = null;
       if (isNotBlank(invalidityDateS)) {
         invalidityDate = DateUtil.parseUtcTimeyyyyMMddhhmmss(invalidityDateS);
       }
+
+      ReqInfo reqInfo = getReqInfo();
+
+      Map<String, CertIdOrError> certIdOrErrors;
 
       ReqRespDebug debug = getReqRespDebug();
       try {
         Requestor requestor = getRequestor();
         X509Cert caCert = getCaCert();
 
-        if (certFile != null) {
-          X509Cert cert = X509Util.parseCert(new File(certFile));
-          certIdOrError = client.revokeCert(caName, requestor, caCert, cert,
-              crlReason.getCode(), invalidityDate, debug);
-        } else {
-          certIdOrError = client.revokeCert(caName, requestor, caCert, getSerialNumber(),
-              crlReason.getCode(), invalidityDate, debug);
+        RevokeCertRequest req = new RevokeCertRequest();
+
+        for (int i = 0; i < reqInfo.ids.size(); i++) {
+          RevokeCertRequest.Entry entry = new RevokeCertRequest.Entry(
+              reqInfo.ids.get(i), caCert.getSubject(), reqInfo.serialNumbers.get(i),
+              crlReason.getCode(), invalidityDate);
+          req.addRequestEntry(entry);
         }
+
+        certIdOrErrors = client.revokeCerts(caName, requestor, req, debug);
       } finally {
         saveRequestResponse(debug);
       }
 
-      if (certIdOrError.getError() != null) {
-        PkiStatusInfo error = certIdOrError.getError();
-        throw new CmdFailure("revocation failed: " + error);
-      } else {
-        println("revoked certificate");
-      }
+      analyseResult(true, certIdOrErrors, reqInfo);
       return null;
     } // method execute0
 
@@ -98,32 +106,29 @@ public class UnRevokeCertActions {
 
     @Override
     protected Object execute0() throws Exception {
-      if ((certFile == null) == (getSerialNumber() == null)) {
-        throw new IllegalCmdParamException("exactly one of cert and serial must be specified");
-      }
+      ReqInfo reqInfo = getReqInfo();
 
       ReqRespDebug debug = getReqRespDebug();
-      CertIdOrError certIdOrError;
+      Map<String, CertIdOrError> certIdOrErrors;
+
       try {
         Requestor requestor = getRequestor();
-        X509Cert caCert = getCaCert();
+        X509Cert caCert = reqInfo.caCert;
 
-        if (certFile != null) {
-          X509Cert cert = X509Util.parseCert(new File(certFile));
-          certIdOrError = client.unsuspendCert(caName, requestor, caCert, cert, debug);
-        } else {
-          certIdOrError = client.unsuspendCert(caName, requestor, caCert, getSerialNumber(), debug);
+        UnrevokeCertRequest req = new UnrevokeCertRequest();
+
+        for (int i = 0; i < reqInfo.ids.size(); i++) {
+          UnrevokeCertRequest.Entry entry = new UnrevokeCertRequest.Entry(
+              reqInfo.ids.get(i), caCert.getSubject(), reqInfo.serialNumbers.get(i));
+          req.addRequestEntry(entry);
         }
+
+        certIdOrErrors = client.unsuspendCerts(caName, requestor, req, debug);
       } finally {
         saveRequestResponse(debug);
       }
 
-      if (certIdOrError.getError() != null) {
-        PkiStatusInfo error = certIdOrError.getError();
-        throw new CmdFailure("releasing revocation failed: " + error);
-      } else {
-        println("unsuspended certificate");
-      }
+      analyseResult(false, certIdOrErrors, reqInfo);
       return null;
     } // method execute0
 
@@ -135,28 +140,105 @@ public class UnRevokeCertActions {
     @Completion(FileCompleter.class)
     private String caCertFile;
 
-    @Option(name = "--cert", aliases = "-c", description = "certificate file (either cert or serial must be specified)")
+    @Option(name = "--cert", aliases = "-c", multiValued = true,
+        description = "certificate files (either cert or serial is allowed)")
     @Completion(FileCompleter.class)
-    protected String certFile;
+    protected List<String> certFiles;
 
-    @Option(name = "--serial", aliases = "-s", description = "serial number (either cert or serial must be specified)")
-    private String serialNumberS;
+    @Option(name = "--serial", aliases = "-s", multiValued = true,
+        description = "serial numbers (either cert or serial is allowed)")
+    private List<String> serialNumbersS;
 
-    private BigInteger serialNumber;
+    private List<BigInteger> serialNumbers;
 
     protected X509Cert getCaCert() throws CertificateException, IOException {
       return X509Util.parseCert(new File(caCertFile));
     }
 
-    protected BigInteger getSerialNumber() {
-      if (serialNumber == null) {
-        if (isNotBlank(serialNumberS)) {
-          this.serialNumber = toBigInt(serialNumberS);
+    protected ReqInfo getReqInfo()
+        throws IllegalCmdParamException, CertificateException, IOException, CmpClientException {
+      if (CollectionUtil.isEmpty(certFiles) && CollectionUtil.isEmpty(serialNumbersS)) {
+        throw new IllegalCmdParamException("none of cert and serial is specified");
+      }
+
+      List<String> ids = new LinkedList<>();
+      List<String> sources = new LinkedList<>();
+      List<BigInteger> serialNumbers = new LinkedList<>();
+
+      X509Cert caCert = getCaCert();
+
+      int id = 1;
+      if (CollectionUtil.isNotEmpty(certFiles)) {
+        for (String certFile : certFiles) {
+          X509Cert cert = X509Util.parseCert(new File(certFile));
+          assertIssuedByCa(cert, caCert, certFile);
+          ids.add(Integer.toString(id++));
+          sources.add(certFile);
+          serialNumbers.add(cert.getSerialNumber());
         }
       }
-      return serialNumber;
+
+      if (CollectionUtil.isNotEmpty(serialNumbersS)) {
+        for (String serialNumber : serialNumbersS) {
+          ids.add(Integer.toString(id++));
+          sources.add(serialNumber);
+          serialNumbers.add(toBigInt(serialNumber));
+        }
+      }
+
+      ReqInfo reqInfo = new ReqInfo();
+      reqInfo.caCert = caCert;
+      reqInfo.ids = ids;
+      reqInfo.sources = sources;
+      reqInfo.serialNumbers = serialNumbers;
+      return reqInfo;
     }
 
-  } // class UnRevRemoveCertAction
+    protected void analyseResult(boolean revoke, Map<String, CertIdOrError> certIdOrErrors,
+                                 ReqInfo reqInfo) throws CmdFailure {
+      boolean failed = false;
+      List<Integer> processedIndex = new ArrayList<>(reqInfo.sources.size());
+      for (Map.Entry<String, CertIdOrError> certIdOrError : certIdOrErrors.entrySet()) {
+        String id = certIdOrError.getKey();
+        int index = reqInfo.ids.indexOf(id);
+
+        if (index == -1) {
+          failed = true;
+          println("error in CMP protocol, unknown id " + id);
+        } else {
+          processedIndex.add(index);
+          String source = reqInfo.sources.get(index);
+          if (certIdOrError.getValue().getError() != null) {
+            failed = true;
+            PkiStatusInfo error = certIdOrError.getValue().getError();
+            println((revoke ? "revoking" : "unsuspending") + " certificate " + source + " failed: " + error);
+          } else {
+            println((revoke ? "revoked" : "suspended") + " certificate " + source);
+          }
+        }
+      }
+
+      if (reqInfo.sources.size() != processedIndex.size()) {
+        Collections.sort(processedIndex, Collections.reverseOrder());
+        for (Integer index : processedIndex) {
+          reqInfo.sources.remove((int) index);
+        }
+        failed = true;
+        println("server did not process request for " + reqInfo.sources);
+      }
+
+      if (failed) {
+        throw new CmdFailure("failed processing at least one certificate");
+      }
+    }
+
+  }
+
+  private static void assertIssuedByCa(X509Cert cert, X509Cert ca, String certDesc) throws CmpClientException {
+    boolean issued = X509Util.issues(ca, cert);
+    if (!issued) {
+      throw new CmpClientException("certificate " + certDesc + "is not issued by the CA");
+    }
+  }
 
 }
